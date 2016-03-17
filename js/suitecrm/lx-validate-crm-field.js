9bb58382b70c97727357da80485036b4f18265dc
Cristian Umaña <cumana@lionix.com>
2016-03-17 10:02:38 -0600

    feat: fnCallerName

--- js/suitecrm/lx-validate-crm-field.js
+++ js/suitecrm/lx-validate-crm-field.js
@@ -1,11 +1,12 @@
-function lxValidateCRMfield(form_name, field_name, label, validate) {
+function lxValidateCRMfield(form_name, field_name, label, validate, fnCallerName = "") {
+    fnCallerName = (fnCallerName != "") ? "(Function " + fnCallerName + ")" : "";
     if (validate) {
-        console.log("lxValidateCRMfield adding validation on form " + form_name + " to field " + field_name);
+        console.log("lxValidateCRMfield adding validation on form " + form_name + " to field " + field_name, fnCallerName);
         //addToValidate is defined somewhere on suitecrm
         addToValidate(form_name, field_name, 'varchar', true, "Falta campo requerido: " + label);
         $('#' + field_name + '_label').html(label + ': <font color="red">*</font>');
     } else {
-        console.log("lxValidateCRMfield removing validation on form " + form_name + " to field " + field_name);
+        console.log("lxValidateCRMfield removing validation on form " + form_name + " to field " + field_name, fnCallerName);
         //removeFromValidate is defined somewhere on suitecrm
         if (validate[form_name] !== undefined) {
             removeFromValidate(form_name, field_name);
